231a48d4af58a103c8910ae31a52e8afd7c898f8
ariadna.b <ariadna.llanos.mt@usa.edu.co>
2022-10-13 21:05:19 -0500

    ultimo commit

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -16,10 +16,11 @@ public class Client implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idClient;
-    private String name;
     private String email;
-    private Integer age;
     private String password;
+    private String name;
+    private Integer age;
+
 
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "client")
     @JsonIgnoreProperties({"client","audience"})
@@ -33,16 +34,8 @@ public class Client implements Serializable {
         return idClient;
     }
 
-    public void setIdClient(Integer id) {
-        this.idClient = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
     }
 
     public String getEmail() {
@@ -53,20 +46,28 @@ public class Client implements Serializable {
         this.email = email;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public List<Messages> getMessages() {
